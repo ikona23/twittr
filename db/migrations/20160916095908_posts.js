2c049ad8d1f008f27cfa91a1e4b23f939783657b
Apprentice <apprentice@StMac0-7.local>
2016-09-16 11:56:31 +1200

    startingcss

--- db/migrations/20160916095908_posts.js
+++ db/migrations/20160916095908_posts.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('posts', function (table) {
     table.increments('id').primary()
     table.string('body')
+    table.dateTime('created_at')
     table.integer('user_id')
   })
 };
